9f8d513668dceaaaf55dd5f501dc01661c3ee2aa
bgcngm <bgcngm@gmail.com>
2019-05-06 12:08:17 -0700

    Daisy: Build system variant of LiveDisplay HAL

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
     $(LOCAL_PATH)/keylayout/sdm845-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sdm845-tavil-snd-card_Button_Jack.kl
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    lineage.livedisplay@1.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
