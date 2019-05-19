3692f88aec65a6d7ea0ad3f7338ec0116472063f
saksham033 <sakshamgoyal02@gmail.com>
2019-05-19 12:10:11 -0700

    Daisy: Add XiaomiParts
    Signed-off-by: saksham033 <sakshamgoyal02@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,10 @@ PRODUCT_PACKAGES += \
     libvulkan \
     vendor.display.config@1.0
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
