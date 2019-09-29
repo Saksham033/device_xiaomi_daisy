b0c088272869bc37657bb5e5ef170b73f73e546c
nebrassy <nebras30@gmail.com>
2019-09-29 19:42:25 +0530

    Daisy:[SQUASHED] Drop composer from matrix and Use Tissot's Build FP
    * These instances should be removed from DT bcz they're present in a wrong way.
    * Also Update Build fingerprint to fix broken safety net.

--- aosp_daisy.mk
+++ aosp_daisy.mk
@@ -1,30 +1,44 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2019 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from Daisy device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common PE stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# PE stuff
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_daisy
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
+PRODUCT_NAME := aosp_daisy
+BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
 PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.3.0.PEJMIXM:user/release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V10.2.3.0.PEJMIXM release-keys" \
-    PRODUCT_NAME="daisy" \
-    TARGET_DEVICE="daisy"
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
